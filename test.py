070f4f4eebac29fdfb749794416736a26b030e3d
Yann-Edwin Keta <yketa@posteo.net>
2023-11-03 18:34:58 +0100

    test T1

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from cells.system import System
+from cells.system import VertexModel
 
 import numpy as np
 
@@ -9,9 +9,8 @@ from matplotlib.colors import Normalize as ColorsNormalise
 from matplotlib.cm import ScalarMappable
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-m = System()
+m = VertexModel()
 m.initRegularTriangularLattice(size=6)
-print(m.getVertexToNeighboursArea(0)); exit()
 
 cax = make_axes_locatable(m.ax).append_axes('right', size='5%', pad=0.05)
 cmap = plt.cm.PiYG
@@ -23,49 +22,53 @@ def update(iterations=100, dt=5e-3, A=5e-2):
 
 	for iteration in range(iterations):
 
-		# compute forces
-
-		forces = {vertex: np.array([0., 0.]) for vertex in m.vertices}
-
-		for halfEdgeIndex in m.halfEdges:
-			if halfEdgeIndex in m.junctions:	# loop over junctions
-
-				disp = m.getHalfEdgeVector(halfEdgeIndex, unit=True)	# normalised vector out of vertex
-
-				amp = np.cos(
-					m.junctions[halfEdgeIndex].w*m.time
-					+ m.junctions[halfEdgeIndex].phi)
-				forces[fromIndex] += A*disp*amp
-				m.junctions[halfEdgeIndex].color = scalarMap.to_rgba(amp)
-
-		# integrate positions
-
-		for vertex in m.vertices:
-
-			m.vertices[vertex].position += forces[vertex]*dt
-			m.vertices[vertex].position = m.wrap(m.vertices[vertex].position)
-
-		# move cell centres
-
-		for vertex in m.cells:
-			fromPos = m.vertices[vertex].position.copy()
-
-			neighbours, _ = m.getNeighbours(vertex, junction=False)
-			for neighbourVertexIndex in range(neighbours):
-				toPos = m.vertices[neighbourVertexIndex].position
-
-				disp = m.wrapDiff(fromPos, toPos)
-				m.vertices[vertex].position += disp/len(neighbours)
-
-			m.vertices[vertex].position = m.wrap(m.vertices[vertex].position)
+# 		# compute forces
+# 
+# 		forces = {vertex: np.array([0., 0.]) for vertex in m.vertices}
+# 
+# 		for halfEdgeIndex in m.halfEdges:
+# 			if halfEdgeIndex in m.junctions:	# loop over junctions
+# 
+# 				disp = m.getHalfEdgeVector(halfEdgeIndex, unit=True)	# normalised vector out of vertex
+# 
+# 				amp = np.cos(
+# 					m.junctions[halfEdgeIndex].w*m.time
+# 					+ m.junctions[halfEdgeIndex].phi)
+# 				forces[fromIndex] += A*disp*amp
+# 				m.junctions[halfEdgeIndex].color = scalarMap.to_rgba(amp)
+# 
+# 		# integrate positions
+# 
+# 		for vertex in m.vertices:
+# 
+# 			m.vertices[vertex].position += forces[vertex]*dt
+# 			m.vertices[vertex].position = m.wrap(m.vertices[vertex].position)
+# 
+# 		# move cell centres
+# 
+# 		for vertex in m.cells:
+# 			fromPos = m.vertices[vertex].position.copy()
+# 
+# 			neighbours, _ = m.getNeighbours(vertex, junction=False)
+# 			for neighbourVertexIndex in range(neighbours):
+# 				toPos = m.vertices[neighbourVertexIndex].position
+# 
+# 				disp = m.wrapDiff(fromPos, toPos)
+# 				m.vertices[vertex].position += disp/len(neighbours)
+# 
+# 			m.vertices[vertex].position = m.wrap(m.vertices[vertex].position)
 
 		# update time
 
 		m.time += dt
+		m.doT1(delta=0, epsilon=0.5)
 
 	# plot
 
 	m.plot()
+	m.fig.suptitle('t=%s' % m.time)
+
+	if m.time > 1: m.initRegularTriangularLattice(size=6)
 
 anim = animation.FuncAnimation(m.fig, update, repeat=True, interval=0)
 plt.show()
