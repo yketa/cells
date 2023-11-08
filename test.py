300311663635a2f12e45dbe99f5aa7ea9723955b
Yann-Edwin Keta <yketa@posteo.net>
2023-11-08 10:54:37 +0100

    mpl Colorbar.__init__ args

--- test.py
+++ test.py
@@ -15,7 +15,8 @@ m.initRegularTriangularLattice(size=6)
 cax = make_axes_locatable(m.ax).append_axes('right', size='5%', pad=0.05)
 cmap = plt.cm.PiYG
 norm = ColorsNormalise(-1, 1)
-colormap = mpl.colorbar.ColorbarBase(cax, cmap, norm, orientation='vertical')
+colormap = mpl.colorbar.ColorbarBase(cax,
+    cmap=cmap, norm=norm, orientation='vertical')
 scalarMap = ScalarMappable(norm, cmap)
 
 def update(iterations=100, dt=1e-3, A=5e-2):
