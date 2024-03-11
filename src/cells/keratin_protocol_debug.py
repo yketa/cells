8711b66426a12a0b9ca53e4707e4514d4fc0ae09
Silke Henkes <silke@Silkes-MacBook-Pro.local>
2024-03-11 18:03:02 +0100

    Constructing suitable protocol for larger system

--- src/cells/keratin_protocol_debug.py
+++ src/cells/keratin_protocol_debug.py
@@ -21,13 +21,13 @@ import sys
 A0 = (3./2.)/np.tan(np.pi/6.)           # area of a regular hexagon with edge length 1
 
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-parser.add_argument("-N", type=int, default=36)
-parser.add_argument("-p0", type=float, default=3.72,help="dimensionless target perimeter of cell")
+parser.add_argument("-N", type=int, default=400)
+parser.add_argument("-p0", type=float, default=3.81,help="dimensionless target perimeter of cell")
  # active forces
 parser.add_argument("-taup", type=float, default=1e0,help="persistence time of active force")
 # active Brownian force
-parser.add_argument("-v0", type=float, default=0.05,help="vertex self-propulsion velocity")
-parser.add_argument("-Fpull", type=float, default=0.05,help="outer pulling force")
+parser.add_argument("-v0", type=float, default=0.1,help="vertex self-propulsion velocity")
+parser.add_argument("-Fpull", type=float, default=0.1,help="outer pulling force")
 
 
 args = parser.parse_args()
@@ -36,12 +36,12 @@ vm.initOpenRegularHexagonalLattice(nCells=args.N)
 vm.addPerimeterForce("perimeter",1, args.p0*np.sqrt(A0))
 vm.addAreaForce("area",1, A0)
 vm.addActiveBrownianForce("abp",args.v0, args.taup)
-#vm.addEdgePullForce("pull",args.Fpull)
+vm.addEdgePullForce("pull",-0.1)
 
 fig, ax = plot(vm)
 plt.ion()
 plt.show()
-for k in range(10):
+for k in range(100):
     vm.nintegrate(100, 0.01, 0.1, 0.11)
     plot(vm, fig=fig, ax=ax)
 
